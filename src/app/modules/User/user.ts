614d52a8987ca8f9802248f320bb2d193e27d3f9
Ashiqur Rahman <imashiqe@gmail.com>
2025-05-16 15:22:53 +0600

    User Route added

---
+++ src/app/modules/User/user.ts
@@ -0,0 +1,10 @@
+import e from "express";
+import express, { Request, Response } from "express";
+
+const router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.send("User route");
+});
+
+export const userRoutes = router;
