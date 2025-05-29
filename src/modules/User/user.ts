022954a73073d0678920d96a7ea18f3514d852f2
Ashiqur Rahman <imashiqe@gmail.com>
2025-05-29 13:20:42 +0600

    modules added

---
+++ src/modules/User/user.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from "express";
+
+const router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "Route is Working Perfectly!",
+  });
+});
