92e6ec96368e09406f17ca6f969fac6f4fb5650a
Ashiqur Rahman <imashiqe@gmail.com>
2025-05-29 15:23:40 +0600

    user created success

--- src/modules/User/user.service.ts
+++ src/modules/User/user.service.ts
@@ -1,7 +1,30 @@
-const createAdmin = async () => {
-  return {
-    message: "Admin created successfully",
+import { PrismaClient, UserRole } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const createAdmin = async (data: any) => {
+  console.log("Creating admin with data:", data);
+  const userData = {
+    email: data.admin.email,
+    password: data.password,
+    role: UserRole.ADMIN,
   };
+
+  //   const adminData = {
+  //     name: data.admin.name,
+  //     email: data.admin.email,
+  //     contactNumber: data.admin.contactNumber,
+  //   }
+  const result = await prisma.$transaction(async (transactionClient) => {
+    const createdUserData = await transactionClient.user.create({
+      data: userData,
+    });
+    const createdAdminData = await transactionClient.admin.create({
+      data: data.admin,
+    });
+    return createdAdminData;
+  });
+
+  return result;
 };
 
 export const userService = {
