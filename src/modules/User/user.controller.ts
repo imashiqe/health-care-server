a543a3bcc942895b33377a2a9dd32c6fdb35449f
Ashiqur Rahman <imashiqe@gmail.com>
2025-05-30 15:25:15 +0600

    success message adde for admin

--- src/modules/User/user.controller.ts
+++ src/modules/User/user.controller.ts
@@ -4,7 +4,11 @@ import { userService } from "./user.service";
 const createAdmin = async (req: Request, res: Response) => {
   //   console.log(req.body);
   const result = await userService.createAdmin(req.body);
-  res.send(result);
+  res.status(200).json({
+    success: true,
+    message: "Admin created successfully",
+    data: result,
+  });
 };
 
 export const userController = {
