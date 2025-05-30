6c3ca582054aa3510328ee1a63ca32affd0765c5
Ashiqur Rahman <imashiqe@gmail.com>
2025-05-30 15:51:56 +0600

    admin fetch from db

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { userRoutes } from "./modules/User/user.routes";
+import { AdminRoutes } from "./modules/Admin/admin.routes";
 
 const app: Application = express();
 
@@ -15,5 +16,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/admin", AdminRoutes);
 
 export default app;
--- src/modules/Admin/admin.routes.ts
+++ src/modules/Admin/admin.routes.ts
@@ -1,5 +1,15 @@
-import express from "express";
+import { PrismaClient } from "@prisma/client";
+import express, { Request, Response } from "express";
 
 const router = express.Router();
+const prisma = new PrismaClient();
+router.get("/", async (req: Request, res: Response) => {
+  const result = await prisma.admin.findMany();
+  res.status(200).json({
+    success: true,
+    message: "Admins fetched successfully",
+    data: result,
+  });
+});
 
 export const AdminRoutes = router;
