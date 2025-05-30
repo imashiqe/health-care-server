a867e4f03a17daaeab9055e6c1d590d43e387486
Ashiqur Rahman <imashiqe@gmail.com>
2025-05-30 16:04:52 +0600

    call  services from  controller

--- src/modules/Admin/admin.controller.ts
+++ src/modules/Admin/admin.controller.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
+import { adminService } from "./admin.service";
 
 const getAllFromDB = async (req: Request, res: Response) => {
+  const result = await adminService.getAllFromDB();
   res.status(200).json({
     success: true,
     message: "Admins fetched successfully",
+    data: result,
   });
 };
 
