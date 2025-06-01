cc8b059a68a9d1c9e2b115ab4dc5d5d36cedfe14
Ashiqur Rahman <imashiqe@gmail.com>
2025-06-01 14:31:09 +0600

    code optimization

--- src/modules/Admin/admin.service.ts
+++ src/modules/Admin/admin.service.ts
@@ -4,25 +4,10 @@ const prisma = new PrismaClient();
 
 const getAllFromDB = async (params: any) => {
   const andConditions: Prisma.AdminWhereInput[] = [];
-
-  // [
-  //         {
-  //           name: {
-  //             contains: params.searchTerm,
-  //             mode: "insensitive",
-  //           },
-  //         },
-  //         {
-  //           email: {
-  //             contains: params.searchTerm,
-  //             mode: "insensitive",
-  //           },
-  //         },
-  //       ],
-
+  const adminSearchAbleFields = ["name", "email"];
   if (params.searchTerm) {
     andConditions.push({
-      OR: ["name", "email"].map((field) => ({
+      OR: adminSearchAbleFields.map((field) => ({
         [field]: {
           contains: params.searchTerm,
           mode: "insensitive",
