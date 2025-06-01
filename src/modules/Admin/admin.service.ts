9c71e7632bffdab080995ee63a93eeaca79bf0f2
Ashiqur Rahman <imashiqe@gmail.com>
2025-06-01 16:25:39 +0600

    filter data

--- src/modules/Admin/admin.service.ts
+++ src/modules/Admin/admin.service.ts
@@ -3,8 +3,10 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getAllFromDB = async (params: any) => {
+  const { searchTerm, ...filterData } = params;
   const andConditions: Prisma.AdminWhereInput[] = [];
   const adminSearchAbleFields = ["name", "email"];
+  console.log(filterData);
   if (params.searchTerm) {
     andConditions.push({
       OR: adminSearchAbleFields.map((field) => ({
@@ -15,7 +17,15 @@ const getAllFromDB = async (params: any) => {
       })),
     });
   }
-
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
+        },
+      })),
+    });
+  }
   //   console.dir(andConditions, { depth: "infinity" });
   const whereConditions: Prisma.AdminWhereInput = { AND: andConditions };
   const result = await prisma.admin.findMany({
