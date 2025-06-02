4f3af300ac011b3c91c6d7b1466414eb947c2b6c
Ashiqur Rahman <imashiqe@gmail.com>
2025-06-02 14:57:59 +0600

    added constant for adminfilter

--- src/modules/Admin/admin.controller.ts
+++ src/modules/Admin/admin.controller.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
 import { adminService } from "./admin.service";
 import pick from "../shared/pick";
+import { adminFilterableFields } from "./admin.constant";
 
 const getAllFromDB = async (req: Request, res: Response) => {
   try {
-    const filters = pick(req.query, ["name", "email", "searchTerm"]);
+    const filters = pick(req.query, adminFilterableFields);
     const result = await adminService.getAllFromDB(req.query);
     res.status(200).json({
       success: true,
