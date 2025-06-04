add6a1371931d21b345ead10f8976d4cc9b2ed92
Ashiqur Rahman <imashiqe@gmail.com>
2025-06-04 17:02:48 +0600

    sorting pagination

--- src/modules/Admin/admin.service.ts
+++ src/modules/Admin/admin.service.ts
@@ -3,8 +3,30 @@ import { adminSearchAbleFields } from "./admin.constant";
 
 const prisma = new PrismaClient();
 
+const calculatePagination = (options: {
+  page?: number;
+  limit?: number;
+  sortBy?: string;
+  sortOrder?: string;
+}) => {
+  const page: number = Number(options.page) || 1;
+  const limit: number = Number(options.limit) || 10;
+  const skip: number = (Number(page) - 1) * limit;
+
+  const sortBy: string = options.sortBy || "createdAt";
+  const sortOrder: string = options.sortOrder || "desc";
+
+  return {
+    page,
+    skip,
+    limit,
+    sortBy,
+    sortOrder,
+  };
+};
+
 const getAllFromDB = async (params: any, options: any) => {
-  const { limit, page } = options;
+  const { page, skip, limit } = calculatePagination(options);
   const { searchTerm, ...filterData } = params;
   const andConditions: Prisma.AdminWhereInput[] = [];
 
@@ -32,8 +54,8 @@ const getAllFromDB = async (params: any, options: any) => {
   const whereConditions: Prisma.AdminWhereInput = { AND: andConditions };
   const result = await prisma.admin.findMany({
     where: whereConditions,
-    skip: (Number(page) - 1) * limit,
-    take: Number(limit),
+    skip,
+    take: limit,
     orderBy:
       options.sortBy && options.sortOrder
         ? {
