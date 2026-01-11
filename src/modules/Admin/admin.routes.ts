22e9267b01b8e476abec992ce23b77cf5ceee359
Ashiqur Rahman <imashiqe@gmail.com>
2026-01-11 17:08:27 +0600

    add as middlewere zod

--- src/modules/Admin/admin.routes.ts
+++ src/modules/Admin/admin.routes.ts
@@ -1,11 +1,29 @@
-import express, { Request, Response } from "express";
+import { AnyZodObject } from "./../../../node_modules/zod/src/v3/types";
+import express, { NextFunction, Request, Response } from "express";
 import { adminController } from "./admin.controller";
+import z from "zod";
 
 const router = express.Router();
 
+const update = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    contactNumber: z.string().optional(),
+  }),
+});
+const validateRequest = (schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
 router.get("/", adminController.getAllFromDB);
 router.get("/:id", adminController.getByIdFromDB);
-router.patch("/:id", adminController.updateIntoDB);
+router.patch("/:id", validateRequest(update), adminController.updateIntoDB);
 router.delete("/:id", adminController.deleteFromDB);
 router.patch("/soft/:id", adminController.softDeleteFromDB);
 export const AdminRoutes = router;
