63763c7bd0330b0c5e76995665d1fa1d7171cdef
Ashiqur Rahman <imashiqe@gmail.com>
2026-01-22 16:30:18 +0600

    jwt installed

--- src/modules/Auth/auth.service.ts
+++ src/modules/Auth/auth.service.ts
@@ -1,5 +1,16 @@
+import * as bcrypt from "bcrypt";
+import prisma from "../shared/prisma";
+
 const loginUser = async (payload: { email: string; password: string }) => {
-  console.log("User logged in", payload);
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: { email: payload.email },
+  });
+  const isCorrectPassword: boolean = await bcrypt.compare(
+    payload.password,
+    userData.password,
+  );
+  console.log(isCorrectPassword);
+  return userData;
 };
 
 export const AuthServices = {
