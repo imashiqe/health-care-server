8bf1a41a724c2dba2c9a0158500fa24e7697b539
Ashiqur Rahman <imashiqe@gmail.com>
2026-02-01 14:18:31 +0600

    implementing role based access on apis

--- src/modules/User/user.routes.ts
+++ src/modules/User/user.routes.ts
@@ -1,8 +1,36 @@
-import express, { Request, Response } from "express";
+import { Secret } from "jsonwebtoken";
+import { error } from "console";
+import express, { NextFunction, Request, Response } from "express";
 import { userController } from "./user.controller";
+import { jwtHelpers } from "../../helpars/jwtHelpers";
+import config from "../../config";
 
 const router = express.Router();
-
-router.post("/", userController.createAdmin);
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new Error("You are not authorized");
+      }
+      const verifiedUser = jwtHelpers.verifyToken(
+        token,
+        config.jwt.jwt_secret as Secret,
+      );
+      console.log(verifiedUser);
+      if (roles.length && roles.includes(verifiedUser.role)) {
+        throw new Error("You are not authorized");
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+router.post(
+  "/",
+  auth("ADMIN", "SUPER_ADMIN", "DOCTOR"),
+  userController.createAdmin,
+);
 
 export const userRoutes = router;
