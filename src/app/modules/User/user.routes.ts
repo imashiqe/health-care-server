132f2ecd09561c7d6d46ae154d94ddd2e828ef05
Ashiqur Rahman <imashiqe@gmail.com>
2026-02-19 16:27:27 +0600

    File Upload Using multer

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -3,12 +3,16 @@ import { userController } from "./user.controller";
 
 import { UserRole } from "@prisma/client";
 import auth from "../../middleWares/auth";
+import multer from "multer";
+import path from "path";
+import { fileUploader } from "../../../helpars/fileUploader";
 
 const router = express.Router();
 
 router.post(
   "/",
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  fileUploader.upload.single("file"),
   userController.createAdmin,
 );
 
